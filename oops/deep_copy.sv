a7c75fc5f1a1275498331e0a4ef065b17de2d9da
shaik <dummy123king@gmail.com>
2025-02-22 21:29:26 +0530

    Updated deep copy

--- oops/deep_copy.sv
+++ oops/deep_copy.sv
@@ -5,6 +5,16 @@ endclass
 class A;
     int j, k;
     B b1; // Nested object
+
+    // Function to perform deep copy
+    function A deep_copy();
+        A copy = new; // Create a new instance of A
+        copy.j = this.j; // Copy primitive properties
+        copy.k = this.k;
+        copy.b1 = new; // Create a new instance of B for the nested object
+        copy.b1.x = this.b1.x; // Copy the nested object's properties
+        return copy;
+    endfunction
 endclass
 
 program test;
@@ -18,12 +28,8 @@ program test;
         a1.b1 = new; // Create nested object
         a1.b1.x = 10;
 
-        // Deep copy: Create a new object a2 and copy properties from a1
-        a2 = new; // Create new object a2
-        a2.j = a1.j; // Copy j from a1 to a2
-        a2.k = a1.k; // Copy k from a1 to a2
-        a2.b1 = new; // Create new nested object for a2
-        a2.b1.x = a1.b1.x; // Copy x from a1.b1 to a2.b1
+        // Perform deep copy of a1 to a2
+        a2 = a1.deep_copy();
 
         // Modify properties of a1 and a2
         a1.j = 5; // Modify a1.j (does not affect a2.j)
@@ -31,5 +37,9 @@ program test;
 
         // Modify nested object through a1
         a1.b1.x = 20; // Does not affect a2.b1.x
+
+        // Display values
+        $display("a1.j = %0d\na1.k = %0d\na1.b1.x = %0d\n", a1.j, a1.k, a1.b1.x);
+        $display("a2.j = %0d\na2.k = %0d\na2.b1.x = %0d\n", a2.j, a2.k, a2.b1.x);
     end
 endprogram
